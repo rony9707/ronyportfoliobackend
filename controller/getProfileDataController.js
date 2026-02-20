820a6cd5749b2da3c9a299e80163ad546cefb6b9
rony9707 <chowdhury.agnibha.98@gmail.com>
2026-02-20 08:11:15 +0530

    find query by _id index

--- controller/getProfileDataController.js
+++ controller/getProfileDataController.js
@@ -1,17 +1,20 @@
-const Profile = require('../models/profile');
+const Profile = require("../models/profile");
 
 exports.getProfile = async (req, res) => {
   try {
-    const profile = await Profile.findOne().lean(); // returns plain JS object
+    const profile = await Profile.findById(
+      "6832fc67c345801c8f85cda3",
+      { _id: 0 }, // exclude _id
+    ).lean();
     if (!profile) {
-      return res.status(404).send({ message: 'Profile not found' });
+      return res.status(404).send({ message: "Profile not found" });
     }
-    
-    delete profile._id;  // remove _id field
-    
+
+    delete profile._id; // remove _id field
+
     res.status(200).json(profile);
   } catch (err) {
-    console.error('Error fetching profile:', err);
-    res.status(500).send({ message: 'Internal Server Error' });
+    console.error("Error fetching profile:", err);
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
